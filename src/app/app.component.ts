66fb0fb3c025c1225be6386afa7531cea13cd52f
oussema.sellami <oussema.sellami@esprit.tn>
2024-02-15 09:28:53 +0100

    routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Residence } from './models/residence';
+import { Apartment } from './models/appartment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstproject';
+
+}
---
+++ src/app/residence/residence.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Apartment } from '../models/appartment';
+import { Residence } from '../models/residence';
+
+@Component({
+  selector: 'app-residence',
+  templateUrl: './residence.component.html',
+  styleUrls: ['./residence.component.css']
+})
+export class ResidenceComponent {
+
+  variable:string="bonjour"
+  prop:boolean=false
+  message:string=""
+ prop1:string="bonjour"
+imageurl="../assets/images/"
+searchsurface!:number
+selectedchoice!:number
+listappart:Apartment[]=[]
+ listResidences:Residence[]=[
+  {id:1,"name": "El fel","address":"Borj Cedria", "image":this.imageurl+"residence1.jpg"},
+   {id:2,"name": "El yasmine", "address":"Ezzahra","image":"../../assets/images/residence3.jpg"},
+   {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/residence3.jpg"},
+   {id:4,"name": "El Anber","address":"Manzah 5", "image":"../../assets/images/residence2.jpg"}
+ ];
+
+ listApartments:Apartment[]=[
+  {id:1,"appartNum":1,"floorNum":0,"surface":100,"terrace":"oui","surfaceTerrace":20,"category":"S+1","description":"Appartement S+1","residence":this.listResidences[0] },
+  {id:2,"appartNum":2,"floorNum":0,"surface":130,"terrace":"non","surfaceTerrace":0,"category":"S+2","description":"Appartement S+2","residence":this.listResidences[0] },
+  {id:3,"appartNum":3,"floorNum":0,"surface":150,"terrace":"oui","surfaceTerrace":30,"category":"S+3","description":"Appartement S+3","residence":this.listResidences[1] },]
+
+
+  showappart(residence:number){
+console.log(residence)
+let applist:Apartment[]=[]
+for(let i=0; i<this.listApartments.length ;i++){
+  if(this.listApartments[i].residence.id==residence){
+applist.push(this.listApartments[i])
+  }
+}
+this.listappart=applist
+  }
+  show(){
+    console.log(this.message="bonjour 4ERP-BI")
+  }
+
+}
